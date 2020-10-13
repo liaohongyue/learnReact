47da93c4ef9143fe59f1a1d014cd9d7882897f00
liaohy <970071213@qq.com>
2020-10-14 00:34:18 +0800

    leart g2plot in home

--- src/Component/G2plotTest.js
+++ src/Component/G2plotTest.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import {Bar} from '@antv/g2plot'
+import {Bar, Line, RingProgress} from '@antv/g2plot'
 import ReactG2Plot from 'react-g2plot';
 
 export default function G2plotTest() {
 
     const data = [
-        { year: '1951 年', value: 38 },
-        { year: '1952 年', value: 52 },
-        { year: '1956 年', value: 61 },
-        { year: '1957 年', value: 145 },
-        { year: '1958 年', value: 48 },
-    ];
+        { year: '1991', value: 3 },
+        { year: '1992', value: 4 },
+        { year: '1993', value: 3.5 },
+        { year: '1994', value: 5 },
+        { year: '1995', value: 4.9 },
+        { year: '1996', value: 6 },
+        { year: '1997', value: 7 },
+        { year: '1998', value: 9 },
+        { year: '1999', value: 13 },
+      ];
 
 
     const options = {
         data,
+        with:100,
+        height:300,
         xField:'year',
         yField:'value',
     };
+
+    const options2 = {
+        height: 300,
+        width: 300,
+        autoFit: false,
+        color: "red",
+        percent:0.7,
+    }
     return (
         <div>
             test
-            <ReactG2Plot Ctor={Bar} options={options} />
+            <ReactG2Plot style={ {width:300, height:300} } Ctor={Line} options={options} />
+            <ReactG2Plot Ctor={RingProgress} options={options2}   />
         </div>
     );
 }
+
